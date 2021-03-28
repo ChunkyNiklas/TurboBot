f7d0a51c6c0699a433ed620a115aabdfe8555d8b
IJxn <hnweiland@we-elektronik.de>
2021-03-28 23:55:46 +0200

    First update

---
+++ src/main/java/net/turbobot/main/Bot.java
@@ -0,0 +1,17 @@
+/**
+ * This code was created by Niklas (Chunky Niklas#0001).
+ * Any unauthorized use of this code is a crime and will be prosecuted accordingly.
+ * Copyright (c) 2021
+ */
+
+package net.turbobot.main;
+
+/*
+ Class: Bot
+ Date: 28.03.2021
+ Coded by Niklas / Chunky Niklas#0001
+*/
+public class Bot {
+
+
+}
